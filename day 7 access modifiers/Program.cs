4c2943a58268a038fa3a2b5c1b60ae7cdb5d0cc0
Sankalpa <sankalpa.gaidhane@resolve.io>
2023-08-12 17:20:48 +0530

    Add project files.

---
+++ day 7 access modifiers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AccessModifierExample
+{
+    public class Person
+    {
+        
+        public string Name { get; private set; }
+
+       
+        private int age;
+
+       
+        public Person(string name, int age)
+        {
+            Name = name;
+            this.age = age;
+        }
+
+       
+        public void Introduce()
+        {
+            Console.WriteLine($"Hi, I'm {Name} and I'm {age} years old.");
+        }
+
+       
+        private void HaveBirthday()
+        {
+            age++;
+            Console.WriteLine("Happy Birthday!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Sankalpa", 24);
+
+            person.Introduce();
+            
+        }
+    }
+}
